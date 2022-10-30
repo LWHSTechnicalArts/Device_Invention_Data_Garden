4a4dbfa27995e0d1a1bd32062b2a79e1a20b2a2e
Andrew Kleindolph <akleindolph@gmail.com>
2022-10-29 18:35:38 -0700

    Create basic_grow_light.py

---
+++ basic_grow_light.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials NeoPixel example"""
+import time
+import board
+import neopixel
+
+pixel_pin = board.A2
+num_pixels = 16
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.7, auto_write=False)
+
+growcolor1 = (180, 0, 255)
+growcolor2 = (180, 200, 255)
+
+while True:
+    pixels.fill(growcolor1)
+    pixels.show()
+    time.sleep(10)
+    pixels.fill(growcolor2)
+    pixels.show()
+    time.sleep(10)
