a17e2b26a3503df1bd839ac2b3704e87f986edcc
Andrew Kleindolph <akleindolph@gmail.com>
2022-11-05 18:22:45 -0700

    Update light_pump_allsensors.py

--- light_pump_allsensors.py
+++ light_pump_allsensors.py
@@ -29,17 +29,16 @@ kit.motor1.throttle = 0  #start with motor off
 
 pixel_pin = board.A2  #neopixel variables
 num_pixels = 16
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.7, auto_write=False)
-growcolor1 = (180, 0, 255)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.9, auto_write=False)
+growcolor1 = (255, 255, 255)
 growcolor2 = (180, 200, 255)
 off = (0, 0, 0)
 
-data_interval = 120
+data_interval = 300  #5 minutes
 
 # Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
 # "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
 # source control.
-# pylint: disable=no-name-in-module,wrong-import-order
 try:
     from secrets import secrets
 except ImportError:
@@ -56,8 +55,6 @@ print("Connecting to %s" % secrets["ssid"])
 wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("Connected to %s!" % secrets["ssid"])
 ### Feeds ###
-
-# Setup a feed named 'photocell' for publishing to a feed
 temperature_feed = secrets["aio_username"] + "/feeds/temperature"
 humidity_feed = secrets["aio_username"] + "/feeds/humidity"
 soil_feed = secrets["aio_username"] + "/feeds/soil"
@@ -130,16 +127,15 @@ print("Connecting to Adafruit IO...")
 mqtt_client.connect()
 
 while True:
-   
+    # Poll the message queue
     for i in range(0,data_interval,1):
         try:
-            # Poll the message queue
             mqtt_client.loop()
         except:
             print ("mqtt fail")
             time.sleep(30)
             pass
-            
+
         if (i!=0) and (i<data_interval):
             time.sleep(1)
         else:
@@ -159,7 +155,9 @@ while True:
             mqtt_client.publish(light_feed, light_value)
             print("Data Published!")
             print(str(data_interval) + ' seconds to next data drop')
-            
+
+            #turn on lights and pump ?
+
             if (light_value < 6500):
                 #print('grow lights on')
                 pixels.fill(growcolor1)
@@ -168,8 +166,8 @@ while True:
                 print('grow lights off')
                 pixels.fill(off)
                 pixels.show()
-    
-        if (moist_value < 70):
-            kit.motor1.throttle = 1
-            time.sleep(20)
-            kit.motor1.throttle = 0
+
+            if (moist_value < 60):
+                kit.motor1.throttle = 1
+                time.sleep(20)
+                kit.motor1.throttle = 0
