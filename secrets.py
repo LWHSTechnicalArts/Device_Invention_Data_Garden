882b6cd5a66fcecda0dd3315255628e2c23d152d
Andrew Kleindolph <akleindolph@gmail.com>
2022-12-26 17:56:28 -0800

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2020 Adafruit Industries
+# SPDX-License-Identifier: Unlicense
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'StudentNet',
+    'password' : '',
+    'aio_username' : 'my_adafruit_io_username',
+    'aio_key' : 'my_adafruit_io_key',
+    'timezone' : "America/Los_Angeles", # http://worldtimeapi.org/timezones
+    }
