89e0c259c2ec39bc26e76652cdadfcb8c2407cfd
Andrew Kleindolph <akleindolph@gmail.com>
2022-12-30 11:16:48 -0800

    Update and rename test_sensor_data.py to post_test_data.py

--- post_test_data.py
+++ post_test_data.py
@@ -1,25 +1,20 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
+#Modified by akleindolph 2022
 
 import time
 import ssl
 import socketpool
 import wifi
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
+import random
 
-# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
-# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
-# source control.
-# pylint: disable=no-name-in-module,wrong-import-order
 try:
     from secrets import secrets
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-# Set your Adafruit IO Username and Key in secrets.py
-# (visit io.adafruit.com if you need to create an account,
-# or if you need your Adafruit IO key.)
 aio_username = secrets["aio_username"]
 aio_key = secrets["aio_key"]
 
@@ -28,28 +23,22 @@ wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("Connected to %s!" % secrets["ssid"])
 ### Feeds ###
 
-# Setup a feed named 'photocell' for publishing to a feed
-photocell_feed = secrets["aio_username"] + "/feeds/photocell"
-
-# Setup a feed named 'onoff' for subscribing to changes
-onoff_feed = secrets["aio_username"] + "/feeds/onoff"
+# Setup a feed named 'test' for publishing to a feed
+test_feed = secrets["aio_username"] + "/feeds/test"
 
 ### Code ###
 
 # Define callback methods which are called when events occur
-# pylint: disable=unused-argument, redefined-outer-name
 def connected(client, userdata, flags, rc):
     # This function will be called when the client is connected
     # successfully to the broker.
-    print("Connected to Adafruit IO! Listening for topic changes on %s" % onoff_feed)
-    # Subscribe to all changes on the onoff_feed.
-    client.subscribe(onoff_feed)
-
+    print("Connected to Adafruit IO! Listening for topic changes on %s" % test_feed)
+    # Subscribe to all changes on the test_feed.
+    client.subscribe(test_feed)    
 
 def disconnected(client, userdata, rc):
     # This method is called when the client is disconnected
     print("Disconnected from Adafruit IO!")
-
 
 def message(client, topic, message):
     # This method is called when a topic the client is subscribed to
@@ -79,14 +68,13 @@ mqtt_client.on_message = message
 print("Connecting to Adafruit IO...")
 mqtt_client.connect()
 
-photocell_val = 0
 while True:
     # Poll the message queue
     mqtt_client.loop()
 
+    random_val = random.randint(0,2000)
     # Send a new message
-    print("Sending photocell value: %d..." % photocell_val)
-    mqtt_client.publish(photocell_feed, photocell_val)
+    print("Sending random value: %d..." % random_val)
+    mqtt_client.publish(test_feed, random_val)
     print("Sent!")
-    photocell_val += 1
-    time.sleep(5)
+    time.sleep(10)
